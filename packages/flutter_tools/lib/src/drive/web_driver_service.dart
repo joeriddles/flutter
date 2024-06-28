498c9604e65473ed99986b42b20c7bca9b82348b
joeriddles <joeriddles10@gmail.com>
2024-06-27 22:41:36 -0700

    Remove unneeded null-aware operation '!'

--- packages/flutter_tools/lib/src/drive/web_driver_service.dart
+++ packages/flutter_tools/lib/src/drive/web_driver_service.dart
@@ -189,7 +189,7 @@ class WebDriverService extends DriverService {
       late int posY = 0;
       if (browserLocation != null) {
         try {
-          posX = int.parse(browserLocation![0]);
+          posX = int.parse(browserLocation[0]);
           posY = int.parse(browserLocation[1]);
         } on FormatException catch (ex) {
           throwToolExit('Dimension provided to --browser-coordinates is invalid: $ex');
