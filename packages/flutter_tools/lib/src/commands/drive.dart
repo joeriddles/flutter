70bdf557590c73bb14a221e84555ebbccb7358ab
joeriddles <joeriddles10@gmail.com>
2024-07-01 09:02:34 -0700

    Add --driver-environment to drive command

--- packages/flutter_tools/lib/src/commands/drive.dart
+++ packages/flutter_tools/lib/src/commands/drive.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:args/args.dart';
 import 'package:meta/meta.dart';
@@ -151,7 +152,9 @@ class DriveCommand extends RunCommandBase {
         help: 'Timeout the test after the given number of seconds. If the '
               '"--screenshot" option is provided, a screenshot will be taken '
               'before exiting. Defaults to no timeout.',
-        valueHelp: '360');
+        valueHelp: '360')
+      ..addOption('driver-environment',
+        help: 'Additional environment variables for the Flutter driver');
   }
 
   final Signals signals;
@@ -305,10 +308,18 @@ class DriveCommand extends RunCommandBase {
         );
       }
 
+      final String? driverEnvironment = stringArg('driver-environment');
+      Map<String, String> environment = <String, String>{};
+      if (driverEnvironment != null) {
+        environment = (jsonDecode(driverEnvironment) as Map<String, dynamic>)
+          .map((String key, dynamic value) => MapEntry<String, String>(key, value.toString())
+        );
+      }
+
       final Future<int> testResultFuture = driverService.startTest(
         testFile,
         stringsArg('test-arguments'),
-        <String, String>{},
+        environment,
         packageConfig,
         chromeBinary: stringArg('chrome-binary'),
         headless: boolArg('headless'),
